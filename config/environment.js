6f5cbe34d145b9bb3a670a2bc60ac24d1876b84c
Piotr Leszkowicz <piotr@bayzat.com>
2024-09-21 16:13:49 +0200

    Updated URL

--- config/environment.js
+++ config/environment.js
@@ -5,12 +5,12 @@ module.exports = function (environment) {
     modulePrefix: 'github-repo-viewer',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'history', // default for development
+
     EmberENV: {
       EXTEND_PROTOTYPES: false,
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
-        // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
       },
     },
 
@@ -20,33 +20,26 @@ module.exports = function (environment) {
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+      // Flags/options to your application instance when it is created
     },
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // Development specific options
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
     ENV.APP.rootElement = '#ember-testing';
     ENV.APP.autoboot = false;
   }
 
   if (environment === 'production') {
-    // here you can enable a production-specific feature
+    // Update the rootURL to match the GitHub Pages subdirectory
+    ENV.rootURL = '/github-repo-viewer/';
+
+    // Use 'hash' location type for GitHub Pages
+    ENV.locationType = 'hash';
   }
 
   return ENV;
