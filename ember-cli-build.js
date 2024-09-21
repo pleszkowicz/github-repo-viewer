d0fdb11dad6db30d86a23d818b479152f7a134c3
Piotr Leszkowicz <piotr@bayzat.com>
2024-09-21 16:35:08 +0200

    Updated URL

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,9 +4,7 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    fingerprint: {
-      prepend: 'https://pleszkowicz.github.io/github-repo-viewer/',
-    },
+    // Add options here
   });
 
   return app.toTree();
